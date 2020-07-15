8847ec1cb10160ebed28786a9228d325fd009a44
Tetsuo Nakamura <tetsuo.nakamura@gmail.com>
2020-07-15 23:49:36 +0900

    Added docker-compose command completion

--- .config/zsh/docker.zsh
+++ .config/zsh/docker.zsh
@@ -1,5 +1,10 @@
 if which docker-compose >/dev/null 2>&1 ; then
   alias dc='docker-compose'
+  if [ ! -f ~/.zsh/completion/_docker-compose ]; then
+    ln -s /Applications/Docker.app/Contents/Resources/etc/docker.zsh-completion ~/.zsh/completion/_docker
+    ln -s /Applications/Docker.app/Contents/Resources/etc/docker-compose.zsh-completion ~/.zsh/completion/_docker-compose
+  fi
+
 else
   echo "Not installed: docker-compose"
 fi
