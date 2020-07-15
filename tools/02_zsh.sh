97e17ec9442accef2b3c05eb28dc04b78c2c584a
Tetsuo Nakamura <tetsuo.nakamura@gmail.com>
2020-07-15 23:54:16 +0900

    Added configration settings of zsh command completion

--- tools/02_zsh.sh
+++ tools/02_zsh.sh
@@ -2,12 +2,19 @@
 
 SEC_ZSH="### zsh local setting"
 
+if [ ! -n ~/.zsh/completion ]; then
+  echo "Create directory for zsh completion file"
+  mkdir -p ~/.zsh/completion
+fi
+
 check_current_sec_zsh=$(grep -c "${SEC_ZSH}" ~/.zshrc 2> /dev/null)
 if [ "${check_current_sec_zsh}" = "0" ]; then
   echo "Add zshrc local setting to ~/.zshrc"
 cat <<EOD >> ~/.zshrc
 ${SEC_ZSH}
 for file in ~/.config/zsh/*.zsh; do source "\${file}"; done
+fpath=(~/.zsh/completion $fpath)
+autoload -Uz compinit && compinit -i
 ${SEC_ZSH}
 EOD
 fi
