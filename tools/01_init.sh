a1001fc5eda5a0b348cf6dbbdacd1ab85cfdfed4
Tetsuo NAKAMURA <nakt@Tetsuos-MacBook-Air.local>
2023-01-01 10:10:15 +0900

    Homebrew installer was rewritten from ruby to bash

--- tools/01_init.sh
+++ tools/01_init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install ansible
